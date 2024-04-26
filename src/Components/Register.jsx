b9f742ffb122f2dc7a9d13b11b5b8f63dc00c5c4
official-MKV <vemmaks84@gmail.com>
2024-04-26 07:56:14 +0100

    register form done

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -4,16 +4,94 @@ import Popup from "./Popup";
 const Register = ({ close }) => {
   return (
     <Popup close={close}>
-      <div className="bg-white w-[500px] h-[400px] rounded-xl">
-        <form>
+      <div className="bg-white w-[40vw] h-[70vh] rounded-xl px-[20px] py-[50px]">
+        <div className="flex flex-wrap gap-5">
           <div>
-            <label>Name</label>
-            <input className="focus:outline-none" />
+            <span className="font-bold text-[17px]">Visitor Details</span>
+            <div>
+              <div className="flex gap-2 w-full">
+                <span>
+                  <label>SurName</label>
+                  <input
+                    placeholder="Surname"
+                    className="focus:outline-none border-[1px] border-black rounded-md w-full px-[5px]"
+                  />
+                </span>
+                <span>
+                  <label>FirstName</label>
+                  <input
+                    placeholder="Surname"
+                    className="focus:outline-none border-[1px] border-black rounded-md w-full px-[5px]"
+                  />
+                </span>
+              </div>
+              <div className="flex gap-2 w-full">
+                <span>
+                  <label>Phone Number</label>
+                  <input
+                    placeholder="phone number"
+                    className="focus:outline-none border-[1px] border-black rounded-md w-full px-[5px]"
+                  />
+                </span>
+                <span>
+                  <label>Email Address</label>
+                  <input
+                    placeholder="Email Address"
+                    className="focus:outline-none border-[1px] border-black rounded-md w-full px-[5px]"
+                  />
+                </span>
+              </div>
+              <div className="flex gap-2 w-full">
+                <span>
+                  <label>Organisation</label>
+                  <input
+                    placeholder="Organisation"
+                    className="focus:outline-none border-[1px] border-black rounded-md w-full px-[5px]"
+                  />
+                </span>
+              </div>
+            </div>
           </div>
-        </form>
+          <div className="flex flex-col gap-3">
+            <span className="font-bold text-[17px]">Staff Details</span>
+            <div>
+              <div className="flex gap-2 w-full">
+                <span>
+                  <label>Whom to See</label>
+                  <input
+                    placeholder="Full Name"
+                    className="focus:outline-none border-[1px] border-black rounded-md w-full px-[5px]"
+                  />
+                </span>
+                <span>
+                  <label>Department</label>
+                  <input
+                    placeholder="Department"
+                    className="focus:outline-none border-[1px] border-black rounded-md w-full px-[5px]"
+                  />
+                </span>
+              </div>
+            </div>
+            <div className="w-full flex items-center gap-2">
+              <label>Cause of Visit:</label>
+              <div className="flex gap-3">
+                <Option value="Excursion" />
+                <Option value="Personal" />
+                <Option value="Marketing" />
+                <Option value="Official" />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </Popup>
   );
 };
-
+const Option = ({ value }) => {
+  return (
+    <div className="rounded-full px-[5px] py-[4px] border-[1px] border-black">
+      <span>{value}</span>
+    </div>
+  );
+};
 export default Register;
